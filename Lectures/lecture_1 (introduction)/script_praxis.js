97a022998e3325c2e9ad2e9317c42e088fc396d9
Egor Pristavka <50916604+mrglaster@users.noreply.github.com>
2022-10-21 00:41:51 +0800

    beutified

--- Lectures/lecture_1 (introduction)/script_praxis.js
+++ Lectures/lecture_1 (introduction)/script_praxis.js
@@ -2,39 +2,38 @@
 
 var operators = ['+', '-', '*', '/', '%'];
 
-function readnumber(numpos){
-    var curnumber = prompt("Введите число номер "+numpos);
+function readnumber(numpos) {
+    var curnumber = prompt("Введите число номер " + numpos);
     curnumber = parseFloat(curnumber);
-    if(isNaN(curnumber)){
+    if (isNaN(curnumber)) {
         alert("Это не число!");
         return readnumber(numpos);
     }
     return curnumber;
 }
 
-function is_operator(value){
-    for(var i =0; i<5; i++){
-        if(value==operators[i]) return true;
+function is_operator(value) {
+    for (var i = 0; i < 5; i++) {
+        if (value == operators[i]) return true;
     }
     return false;
 }
 
-function read_operator(){
+function read_operator() {
     var operator = prompt("Введите оператор");
-    if(is_operator(operator)){
+    if (is_operator(operator)) {
         return operator;
     }
     alert("Не оператор!");
     return read_operator();
 }
 
-function start(){
-       var a = readnumber("1");
-       var b = readnumber("2");
-       var oper = read_operator();
-       var result = eval(a+oper+b);
-       alert(result);
+function start() {
+    var a = readnumber("1");
+    var b = readnumber("2");
+    var oper = read_operator();
+    var result = eval(a + oper + b);
+    alert(result);
 }
 
 start();
-
