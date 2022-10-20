19b9854df0367e002f92e194a78b895c52783de1
Egor Pristavka <50916604+mrglaster@users.noreply.github.com>
2022-10-21 00:47:08 +0800

    Update object_transformation.js

--- Lectures/lecture_2 (arrays, objects, etc)/object_transformation.js
+++ Lectures/lecture_2 (arrays, objects, etc)/object_transformation.js
@@ -1,24 +1,24 @@
-function read_array(){
+function read_array() {
     let current_string = prompt();
     return current_string.split(" ");
 }
 
 
-function get_obj_fromkb(){
+function get_obj_fromkb() {
     let arr_1 = read_array();
     let arr_2 = read_array();
     let result = {};
-    if (arr_1.length == arr_2.length){
-        for(let i=0; i<arr_1.length; i++){
+    if (arr_1.length == arr_2.length) {
+        for (let i = 0; i < arr_1.length; i++) {
             result[arr_1[i]] = arr_2[i];
         }
     }
     return result;
 }
 
-function transform_obj(our_object){
+function transform_obj(our_object) {
     let result_matrix = [];
-    for (const i of Object.keys(our_object)){
+    for (const i of Object.keys(our_object)) {
         let current_array = [];
         current_array.push(i);
         current_array.push(our_object[i]);
