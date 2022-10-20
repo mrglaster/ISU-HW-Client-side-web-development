540b69d10ba7e359ba18711a5151cdfc4bc9365c
Egor Pristavka <50916604+mrglaster@users.noreply.github.com>
2022-10-21 00:44:25 +0800

    Update task_bigint.js

--- Lectures/lecture_4 (bigint)/task_bigint.js
+++ Lectures/lecture_4 (bigint)/task_bigint.js
@@ -1,16 +1,16 @@
-function fast_factorial(n){
-    if(n==1) return 1;
-    return n*fast_factorial(n-1);
+function fast_factorial(n) {
+    if (n == 1) return 1;
+    return n * fast_factorial(n - 1);
 }
 
 
-function solve(n){
+function solve(n) {
     let factor = BigInt(fast_factorial(n));
     let mulrsult = BigInt(1);
-    for(let i=1; i<=n; i++){
-        mulrsult *= BigInt(i+n);
-    }    
-     console.log((mulrsult/factor/BigInt(n+1)).toString());
+    for (let i = 1; i <= n; i++) {
+        mulrsult *= BigInt(i + n);
+    }
+    console.log((mulrsult / factor / BigInt(n + 1)).toString());
 }
 
 number = prompt("Input the number");
