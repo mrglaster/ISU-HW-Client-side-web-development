c535bb270e5686b301c56f1e221ae3e484408a00
Egor Pristavka <50916604+mrglaster@users.noreply.github.com>
2022-10-21 00:47:42 +0800

    Update string_task.js

--- Lectures/lecture_3 (strings)/string_task.js
+++ Lectures/lecture_3 (strings)/string_task.js
@@ -1,21 +1,21 @@
 const str = "ggggggguuujjsjjjjjlllllpppppwpppdddkkkffffapds";
 //Результат: g7u3j2s1j5l5p4w1p3d3k3f4a1p1d1s1
 
-function transform_string(string){
+function transform_string(string) {
     counter = 0;
     current_char = str[0];
     result_string = "";
-    for(let i of str){
-        if (i==current_char) counter++;
-        else{
-            result_string+=current_char;
-            result_string+=counter;
+    for (let i of str) {
+        if (i == current_char) counter++;
+        else {
+            result_string += current_char;
+            result_string += counter;
             current_char = i;
             counter = 1;
         }
     }
-    result_string+=current_char;
-    result_string+=counter;
+    result_string += current_char;
+    result_string += counter;
     return result_string;
 }
 
