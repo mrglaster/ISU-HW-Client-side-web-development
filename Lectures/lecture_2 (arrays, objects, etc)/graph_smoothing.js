b3d745e18864dbba54af02e38cdacb3696ecc96c
Egor Pristavka <50916604+mrglaster@users.noreply.github.com>
2022-10-21 00:42:40 +0800

    Beautified

--- Lectures/lecture_2 (arrays, objects, etc)/graph_smoothing.js
+++ Lectures/lecture_2 (arrays, objects, etc)/graph_smoothing.js
@@ -1,23 +1,23 @@
-function cast2_numarray(i_array){
+function cast2_numarray(i_array) {
     var number_array = [];
-    for(let i=0; i<i_array.length; i++){
+    for (let i = 0; i < i_array.length; i++) {
         number_array.push(parseFloat(i_array[i]));
     }
     return number_array;
 }
 
-function read_array(){
+function read_array() {
     let current_string = prompt();
     return cast2_numarray(current_string.split(" "));
 }
 
-function smooth_array(num_array){
+function smooth_array(num_array) {
     let smoothed_array = [];
     smoothed_array.push(num_array[0]);
-    for(let i=1; i<num_array.length-1; i++){
-        smoothed_array.push(parseFloat((num_array[i]+num_array[i-1]+num_array[i+1])/3));
+    for (let i = 1; i < num_array.length - 1; i++) {
+        smoothed_array.push(parseFloat((num_array[i] + num_array[i - 1] + num_array[i + 1]) / 3));
     }
-    smoothed_array.push(num_array[num_array.length-1]);
+    smoothed_array.push(num_array[num_array.length - 1]);
     return smoothed_array;
 
 }
